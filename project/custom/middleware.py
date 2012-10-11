bfdb33f7ea637d2b00dc7e9efda7d1ce097411bf
Vince Carney <vincecarney@gmail.com>
2012-10-11 15:18:27 -0500

    How 'bout a general Referrer Middleware dude

--- project/custom/middleware.py
+++ project/custom/middleware.py
@@ -1,8 +1,14 @@
-class SourceReferrerMidddleware(object):
+class ReferrerMidddleware(object):
     """
-    Set source referrer on any page
+    Set referrer on any page
     """
     def process_request(self, request):
+        # Source Referrer
         if request.GET.get('sr'):
             request.session['sr'] = request.GET.get('sr')
+
+        # User Referrer
+        if request.GET.get('ur'):
+            request.session['ur'] = request.GET.get('ur')
+
         return None
--- project/project/settings_default.py
+++ project/project/settings_default.py
@@ -115,7 +115,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'custom.middleware.SourceReferrerMidddleware',
+    'custom.middleware.ReferrerMidddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
