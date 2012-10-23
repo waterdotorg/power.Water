ed6d7a747b325aed8296eca03e229f9e23342a04
arnonate <hello@natearnold.me>
2012-10-23 16:43:00 -0500

    Email form work in

--- project/project/public/static/js/main.js
+++ project/project/public/static/js/main.js
@@ -1,1 +1,35 @@
+function apply_form_field_error(fieldname, error) {
+    var input = $("#" + fieldname),
+        container = $("#" + fieldname + "-form"),
+        error_msg = $("<span />").addClass("alert-error ajax-error").text(error[0]);
 
+    container.addClass("error");
+    error_msg.insertAfter(container);
+}
+
+function clear_form_field_errors(form) {
+    $(".ajax-error", $(form)).remove();
+    $(".error", $(form)).removeClass("error");
+}
+
+// Ajax Form animation
+function ajaxFormShow() {
+	$("#ajax-form").slideDown("medium");
+}
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+var csrftoken = getCookie('csrftoken');
